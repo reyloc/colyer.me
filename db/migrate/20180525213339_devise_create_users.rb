b7fd01fade05a43cbc08ff80587e0b490e137865
Jason Colyer <jcolyer2007@gmail.com>
2018-05-25 17:28:32 -0500

    Devise work

--- db/migrate/20180525213339_devise_create_users.rb
+++ db/migrate/20180525213339_devise_create_users.rb
@@ -19,6 +19,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :username,             unique: true
     add_index :users, :reset_password_token, unique: true
   end
 end
